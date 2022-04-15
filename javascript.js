960b5055ac00bf0ad4d059548340656275b18862
afkus3r <sheila.skalli@gmail.com>
2022-04-15 03:44:04 +0200

    Add colorChoice and hover functions

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
-// function sizeChoice () {
-//     const size = document.querySelectorAll('input[name=size]');
-//     size.forEach((size) => size.addEventListener('click'));
-// }
-
 function grid(size) {
   const box = document.querySelector(".box");
+  while (box.firstChild) {
+    box.removeChild(box.lastChild);
+  }
   for (let i = 0; i < size; i++) {
     const rows = document.createElement("div");
     rows.classList.add("rows");
@@ -17,4 +15,33 @@ function grid(size) {
   }
 }
 
+function sizeChoice () {
+  const size = document.querySelectorAll("input[name=size]");
+  size.forEach((size) => {
+    size.addEventListener("click", () => {
+        grid(size.value);
+    });
+  });
+}
+
+function hover (hoverStyle) {
+  const cubes = document.querySelectorAll(".cubes");
+  cubes.forEach((cube) => {
+    cube.addEventListener("mouseover", () => {
+      cube.setAttribute("class", hoverStyle);
+    });
+  });
+}
+
+function colorChoice () {
+  const colors = document.querySelectorAll("input[name=color]");
+  colors.forEach((color) => {
+    color.addEventListener("click", () => {
+       hover(color.value);
+    });
+  });
+}
+
 grid(16);
+sizeChoice();
+colorChoice();
